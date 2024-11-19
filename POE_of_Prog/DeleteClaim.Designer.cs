ecaaa84d163956c01fc33269741ecca748008060
RC_Student_lab <RC_Student_lab@labG9AEB3>
2024-11-19 19:59:27 +0000

    Add Delete window

--- POE_of_Prog/DeleteClaim.Designer.cs
+++ POE_of_Prog/DeleteClaim.Designer.cs
@@ -32,14 +32,12 @@
             label1 = new Label();
             SubmitDeleteClaimID = new Button();
             ExitBtn = new Button();
-            panel1 = new Panel();
             label2 = new Label();
-            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // DeleteClaimIdInput
             // 
-            DeleteClaimIdInput.Location = new Point(579, 125);
+            DeleteClaimIdInput.Location = new Point(392, 125);
             DeleteClaimIdInput.Name = "DeleteClaimIdInput";
             DeleteClaimIdInput.Size = new Size(209, 23);
             DeleteClaimIdInput.TabIndex = 3;
@@ -49,67 +47,61 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(301, 125);
+            label1.Location = new Point(125, 121);
             label1.Name = "label1";
-            label1.Size = new Size(245, 25);
+            label1.Size = new Size(247, 25);
             label1.TabIndex = 4;
-            label1.Text = "Please enter your Claim ID";
+            label1.Text = "Please enter your claim ID:";
             // 
             // SubmitDeleteClaimID
             // 
+            SubmitDeleteClaimID.BackColor = SystemColors.ControlDark;
             SubmitDeleteClaimID.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            SubmitDeleteClaimID.Location = new Point(539, 223);
+            SubmitDeleteClaimID.Location = new Point(347, 179);
             SubmitDeleteClaimID.Name = "SubmitDeleteClaimID";
             SubmitDeleteClaimID.Size = new Size(75, 34);
             SubmitDeleteClaimID.TabIndex = 5;
             SubmitDeleteClaimID.Text = "Submit";
-            SubmitDeleteClaimID.UseVisualStyleBackColor = true;
+            SubmitDeleteClaimID.UseVisualStyleBackColor = false;
             SubmitDeleteClaimID.Click += SubmitDeleteClaimID_Click;
             // 
             // ExitBtn
             // 
+            ExitBtn.BackColor = SystemColors.ControlDark;
             ExitBtn.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            ExitBtn.Location = new Point(104, 404);
+            ExitBtn.Location = new Point(53, 373);
             ExitBtn.Name = "ExitBtn";
             ExitBtn.Size = new Size(75, 34);
             ExitBtn.TabIndex = 6;
             ExitBtn.Text = "Exit";
-            ExitBtn.UseVisualStyleBackColor = true;
+            ExitBtn.UseVisualStyleBackColor = false;
             ExitBtn.Click += ExitBtn_Click;
-            // 
-            // panel1
-            // 
-            panel1.BackColor = SystemColors.Highlight;
-            panel1.Controls.Add(ExitBtn);
-            panel1.Location = new Point(0, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(298, 503);
-            panel1.TabIndex = 7;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(345, 32);
+            label2.Location = new Point(245, 22);
             label2.Name = "label2";
-            label2.Size = new Size(309, 25);
+            label2.Size = new Size(301, 25);
             label2.TabIndex = 8;
-            label2.Text = "Are you sure you want to delete ?";
+            label2.Text = "Do you want to delete the claim?";
             // 
             // DeleteClaim
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Navy;
             ClientSize = new Size(800, 450);
+            Controls.Add(ExitBtn);
             Controls.Add(label2);
-            Controls.Add(panel1);
             Controls.Add(SubmitDeleteClaimID);
             Controls.Add(label1);
             Controls.Add(DeleteClaimIdInput);
+            ForeColor = SystemColors.ControlLight;
             Name = "DeleteClaim";
             Text = "DeleteClaim";
             Load += DeleteClaim_Load;
-            panel1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -120,7 +112,6 @@
         private Label label1;
         private Button SubmitDeleteClaimID;
         private Button ExitBtn;
-        private Panel panel1;
         private Label label2;
     }
 }
